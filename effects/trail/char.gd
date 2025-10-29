0d02683a228c4f83401ef9e942ba451cb4fe5999
Shafayat Bin Mamun <mamunshafayatbin@yahoo.com>
2025-10-29 11:45:01 +0600

    Removed warning

--- effects/trail/char.gd
+++ effects/trail/char.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * 500
 	move_and_slide()
